dcb92a3f9da672287ca6920808a18e5024921748
Shiven-Saurish <113611504+Shiven-Saurish@users.noreply.github.com>
2023-02-09 19:18:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+
+
+function playSound(){
+    x.play();
+    }
+ function stopSound(){
+    x.stop();
+    }
+            
